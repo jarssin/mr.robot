67d4bb44a17c1f0bd5dd7e8141d6056f3b790ca7
igormaia01 <igor.maia@syngenta.com>
2025-05-28 18:50:08 -0500

    fix: closing database connection to avoid problems

--- src/repositories/common/baseRepository.ts
+++ src/repositories/common/baseRepository.ts
@@ -1,54 +1,53 @@
-import { Database } from 'sqlite3'
+import { Database } from 'sqlite3';
 
-import logger from '../../utils/logger'
+import logger from '../../utils/logger';
 
-import { SqliteDB } from '../../providers/database'
+import { SqliteDB } from '../../providers/database';
 
 export class BaseRepository {
-  public db: Database
+  public db: Database;
 
   constructor() {
-    this.db = new SqliteDB().connection
+    this.db = new SqliteDB().connection;
   }
 
   run(sql: string, params: any[] = []): Promise<void> {
     return new Promise((_, rej) => {
       this.db.run(sql, params, (err: any) => {
         if (err) {
-          logger.error(err)
-          rej(err)
+          logger.error(err);
+          rej(err);
         }
-      })
-    })
+      });
+    });
   }
 
-  get<T>(sql: string, params: any[] = []): Promise<T | null>{
+  get<T>(sql: string, params: any[] = []): Promise<T | null> {
     const model = new Promise<T | null>((res, rej) => {
       this.db.get(sql, params, (err: any, result: T) => {
         if (err) {
-          logger.error(err)
-          rej(err)
+          logger.error(err);
+          rej(err);
         }
-
-        res(result)
-      })
-    })
-    this.db.close()
-    return model
+        this.db.close();
+        res(result);
+      });
+    });
+    return model;
   }
 
-  all<T>(sql: string, params: any[] = []): Promise<T[] | []>{
+  all<T>(sql: string, params: any[] = []): Promise<T[] | []> {
     const model = new Promise<T[] | []>((res, rej) => {
       this.db.all(sql, params, (err: any, result: T[]) => {
         if (err) {
-          logger.error(err)
-          rej(err)
+          logger.error(err);
+          rej(err);
         }
 
-        res(result)
-      })
-    })
-    this.db.close()
-    return model
+        res(result);
+      });
+    });
+    this.db.close();
+    return model;
   }
 }
