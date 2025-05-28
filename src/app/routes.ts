34adc83de1c70fef6a62ec23641dc130b80f061d
igormaia01 <igor.maia@syngenta.com>
2025-05-28 12:02:10 -0500

    chore: lint

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,80 +1,70 @@
-import multer from 'multer'
-import path from 'path'
-import Router from 'express-promise-router'
-import { Whatsapp } from 'venom-bot'
+import multer from 'multer';
+import path from 'path';
+import Router from 'express-promise-router';
+import { Whatsapp } from 'venom-bot';
 
-import { MakeRequest } from '../middlewares/makeRequest'
+import { MakeRequest } from '../middlewares/makeRequest';
 
-import { 
-  CreateDto,
-  SendDto,
-  SendAllContactsDto
-} from '../dto'
+import { CreateDto, SendDto, SendAllContactsDto } from '../dto';
 
-import { 
+import {
   CreateController,
   ListCampaignController,
   SendController,
   SendAllContactsController,
-} from '../controllers'
-import { CreateHumanDto } from '../dto/createHuman.dto'
-import { CreateHumansController } from '../controllers/people/createHumans'
+} from '../controllers';
+import { CreateHumanDto } from '../dto/createHuman.dto';
+import { CreateHumansController } from '../controllers/people/createHumans';
 
 const config = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, './tmp/img/')
+    cb(null, './tmp/img/');
   },
   filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname))
-  }
-})
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
 
 const upload = multer({
-  storage: config
-})
+  storage: config,
+});
 
 export const entryPoint = (client: Whatsapp) => {
-  const router = Router()
+  const router = Router();
 
   router.get('/health', (_, res) => {
-    res.status(200).json('Ok')
-  })
+    res.status(200).json('Ok');
+  });
 
-  router.get(
-    '/list',
-    new ListCampaignController().handler
-  )
+  router.get('/list', new ListCampaignController().handler);
 
   router.post(
     '/people',
     MakeRequest.make(CreateDto),
     new CreateController().handler
-  )
+  );
 
   router.post(
     '/human',
     MakeRequest.make(CreateHumanDto),
-    new CreateHumansController()
-      .handler
-  )
+    new CreateHumansController().handler
+  );
 
   router.post(
     '/send',
     upload.single('file'),
     MakeRequest.make(SendDto),
-    new SendController(client)
-      .handler
-      .bind(new SendController(client))
-  )
+    new SendController(client).handler.bind(new SendController(client))
+  );
 
   router.post(
     '/send-to-campaign',
     upload.single('file'),
     MakeRequest.make(SendAllContactsDto),
-    new SendAllContactsController(client)
-      .handler
-      .bind(new SendAllContactsController(client))
-  )
+    new SendAllContactsController(client).handler.bind(
+      new SendAllContactsController(client)
+    )
+  );
 
   // router.get('/get-all', async (_, res) => {
   //   const people = await new ListPeopleRepository().execute()
@@ -92,9 +82,9 @@ export const entryPoint = (client: Whatsapp) => {
 
   //   let response = await Promise.all(contacts)
   //   response = response.filter((resp: any | undefined) => !(resp && resp?.erro ))
-    
+
   //   return res.status(200).json({ body: response })
   // })
 
-  return router
-}
+  return router;
+};
