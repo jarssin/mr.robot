d301e226afa52271d9d7e7b4ae4c04ec57d8ae1b
igormaia01 <igor.maia@syngenta.com>
2025-05-28 12:02:24 -0500

    feat: codition to run in mac os

--- src/providers/whatsapp.ts
+++ src/providers/whatsapp.ts
@@ -1,12 +1,18 @@
-import { create, Whatsapp } from "venom-bot";
+import { create, Whatsapp } from 'venom-bot';
+import os from 'os';
 
 export class WhatsappProvider {
   async connect(): Promise<Whatsapp> {
+    const isMac = os.platform() === 'darwin';
+    const browserPathExecutable = isMac
+      ? '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+      : '/usr/bin/chromium-browser';
+
     const client = await create(
-      "mr_robot",
+      'mr_robot',
       (base64Qrimg, asciiQR, attempts) => {},
       undefined,
-      { headless: "new", browserPathExecutable: "/usr/bin/chromium-browser" }
+      { headless: 'new', browserPathExecutable }
     );
 
     return client;
